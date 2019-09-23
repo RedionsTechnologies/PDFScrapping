fc01871bd6a353752fe26c4d87400bd1ead79bee
ZyedKhan <zaid.khan2592@gmail.com>
2019-09-23 18:52:48 +0530

    Added requirements.txt

--- pdfScrappingMain.py
+++ pdfScrappingMain.py
@@ -1,1 +1,12 @@
 # PYTHON CODE START HERE
+
+import PyPDF2
+
+pdfFileObj = open('demoData.pdf', 'rb')
+
+pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+print(pdfReader.numPages)
+
+pageObj = pdfReader.getPage(0)
+
+print(pageObj.extractText())
