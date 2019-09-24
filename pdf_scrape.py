48364641bc495a7d0c0080baec65513f5de8482d
imvishvaraj <vishvarajdhnwd120@gmail.com>
2019-09-24 16:40:59 +0530

    Modified to extract table

--- pdf_scrape.py
+++ pdf_scrape.py
@@ -2,17 +2,17 @@
 this file will parse data form pdf
 And output will be into json or csv
 """
-from pdfminer.pdfparser import PDFParser
-from pdfminer.pdfdocument import PDFDocument
+# from pdfminer.pdfparser import PDFParser
+# from pdfminer.pdfdocument import PDFDocument
 import tabula
 
 
-def get_metadata_info(path):
-    file = open(path, 'rb')
-    parser = PDFParser(file)
-    doc = PDFDocument(parser)
-
-    return doc.info
+# def get_metadata_info(path):
+#     file = open(path, 'rb')
+#     parser = PDFParser(file)
+#     doc = PDFDocument(parser)
+#
+#     return doc.info
 
 
 def extract_table(path):
@@ -23,7 +23,7 @@ def extract_table(path):
     Error: It saves data as expect checkbox column
             Does not get Column names correctly.
     """
-    directory = path.split("/")[0]
+    directory = "output_data"
     filename = path.split("/")[-1]
     filename = filename.split(".")[0]
     df = tabula.read_pdf(path)
@@ -36,8 +36,8 @@ def get_pdf_data():
 
 
 def main():
-    pass
+    extract_table("input_data/11.pdf")
 
 
-if __name__ == '__manin__':
+if __name__ == '__main__':
     main()
