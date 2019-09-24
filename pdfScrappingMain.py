4427ee34550639472a34f722b616b9d92354131d
ZyedKhan <zaid.khan2592@gmail.com>
2019-09-24 13:46:48 +0530

    Added code, reads from other pdf files except the one given

--- pdfScrappingMain.py
+++ pdfScrappingMain.py
@@ -1,12 +1,31 @@
-# PYTHON CODE START HERE
-
 import PyPDF2
+import tabula
+from pandas import DataFrame
 
-pdfFileObj = open('demoData.pdf', 'rb')
+try:
+    pdfFileObj = open('sample.pdf', 'rb')
+    print("FILE OPENED with Object :", pdfFileObj)
+    pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+    if pdfReader.isEncrypted:
+        print("ENCRYPTED")
+        pdfReader = pdfReader.decrypt("ENTER PASSWORD HERE")
+    print("Number of pages: ", pdfReader.numPages)
+    for i in range(0, pdfReader.numPages):
+        pageObj = pdfReader.getPage(i)
+        print("PDF Data: ", pageObj.extractText())
+    pdfFileObj.close()
+except Exception as ex:
+    print("Exception in opening file: ", ex)
 
-pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
-print(pdfReader.numPages)
-
-pageObj = pdfReader.getPage(0)
-
-print(pageObj.extractText())
+#FOR READING TABLE DATA
+try:
+    # Tabula Information
+    # print(tabula.environment_info())
+    df = tabula.read_pdf("demoData2.pdf")
+    # in order to print first 5 lines of Table
+    # print("Head of PDF", df.head())
+    # df = tabula.read_pdf("demoData2.pdf", multiple_tables = True)
+    # tabula.read_pdf("demoData2.pdf", area=(126, 149, 212, 462), pages=1)
+    # tabula.read_pdf("demoData2.pdf", output_format="json")
+except Exception as ex:
+    print("Exception in opening file: ", ex)
