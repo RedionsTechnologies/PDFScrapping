1d3def0b9a5bae0647a3ac42ee5a5424a1e44c00
Vishvaraj Dhanawade <vishvarajdhnwd120@gmail.com>
2019-11-04 17:21:04 +0530

    updated test code

--- test_dl2.py
+++ test_dl2.py
@@ -59,4 +59,11 @@ import numpy as np
 from keras.preprocessing import image
 test_image = image.load_img('random.jpg', target_size=(64, 64))
 test_image = image.img_to_array(test_image)
-test_image
+test_image = np.expand_dims(test_image, axis=0)
+result = classifier.predict(test_image)
+training_set.class_indices
+if result[0][0] >= 0.5:
+    prediction = 'dog'
+else:
+    prediction = 'cat'
+print(prediction)
