5f9344db4178aaa7a873d3ccc53cb7e5706802b3
Vishvaraj Dhanawade <vishvarajdhnwd120@gmail.com>
2019-11-20 21:26:39 +0530

    detecting object into image

---
+++ data_extraction_img/detect_obinimg.py
@@ -0,0 +1,29 @@
+import os
+import cv2
+import numpy as np
+
+cdir = os.path.join(os.getcwd(), 'tst_papper')
+
+# Load input image and convert it into gray
+img1 = os.path.join(cdir, '2.png')
+image = cv2.imread(img1)
+# cv2.imshow('page', image)         # to view image file
+# cv2.waitKey(0)
+#gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)         # convert to grey scale
+
+# Load the template image
+template = cv2.imread(os.path.join(cdir, 'g.png'))
+tg = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
+
+result = cv2.matchTemplate(image, template, cv2.TM_CCOEFF_NORMED)
+sin_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
+
+# create bounding box
+top_left = max_loc
+# increasing the size of bounding rectangle by 50 pixels
+bottom_right = (top_left[0]+10, top_left[1]+10)
+cv2.rectangle(image, top_left, bottom_right, (0, 255, 0), 5)
+
+cv2.imshow('object found', image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
