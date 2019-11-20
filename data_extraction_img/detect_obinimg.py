a3a0749f2b14324227521a37cecc9a6069fc19b1
Vishvaraj Dhanawade <vishvarajdhnwd120@gmail.com>
2019-11-20 21:47:34 +0530

    modified code

--- data_extraction_img/detect_obinimg.py
+++ data_extraction_img/detect_obinimg.py
@@ -1,19 +1,24 @@
+"""
+Currently this code detects only one image inside image.
+
+THIS CODE NEEDS TO BE RUN LINE BY LINE, LIKE JUPYTER NOTEBOOK
+"""
 import os
 import cv2
 import numpy as np
 
-cdir = os.path.join(os.getcwd(), 'tst_papper')
+cdir = os.path.join(os.getcwd(), 'data_extraction_img')
 
 # Load input image and convert it into gray
-img1 = os.path.join(cdir, '2.png')
+img1 = os.path.join(cdir, '4.png')
 image = cv2.imread(img1)
 # cv2.imshow('page', image)         # to view image file
 # cv2.waitKey(0)
 #gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)         # convert to grey scale
 
 # Load the template image
-template = cv2.imread(os.path.join(cdir, 'g.png'))
-tg = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
+template = cv2.imread(os.path.join(cdir, '1.jpg'))
+# tg = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
 
 result = cv2.matchTemplate(image, template, cv2.TM_CCOEFF_NORMED)
 sin_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
